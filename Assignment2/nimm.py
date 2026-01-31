9d2bce6605f348d695fb2d646b71ec4feb269eac
fanweikung <fan-wei.kung@unicodeconsulting.com>
2026-01-31 15:46:28 -0800

    Assignment 2 Part 3 Solution Nimm/Tsynshidzi game

--- Assignment2/nimm.py
+++ Assignment2/nimm.py
@@ -12,7 +12,31 @@ def main():
     your own code. You should also delete this comment and replace
     it with a better, more descriptive one.
     """
-    pass
+    num_stone_left = 20
+    tPlayers = ("Player 1", "Player 2")
+    current_player = tPlayers[0]
+
+    while num_stone_left > 0: 
+        print(f'There are {num_stone_left} left.')
+        num_stone_removed = int(input(f'{current_player}, would you like to remove 1 or 2 stones? '))
+        # check if user enters a valid number.
+        # if the number is invalid, keep asking user to enter a valid number (1 or 2)
+        while num_stone_removed != 1 and num_stone_removed != 2:
+            num_stone_removed = int(input(f'Please enter 1 or 2: '))
+        num_stone_left -= num_stone_removed
+        # Fencepole problem
+        # get next player returns next player
+        current_player = get_next_player(tPlayers, current_player)
+    current_player = get_next_player(tPlayers, current_player)
+    
+    print("game over")
+    print(f'{current_player}, you lost!')
+
+def get_next_player(tP, p):
+        if p == tP[0]:
+             return tP[1]
+        else:
+            return tP[0]
 
 
 # This provided line is required at the end of a Python file
