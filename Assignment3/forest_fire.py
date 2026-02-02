00ebf9b3765c49eb3306d86f5e4c6065190312e4
fanweikung <fan-wei.kung@unicodeconsulting.com>
2026-02-02 13:31:37 -0800

    Assignment 3 Part 2 Finding Forest Flames Solution

--- Assignment3/forest_fire.py
+++ Assignment3/forest_fire.py
@@ -1,9 +1,9 @@
 """
 File: forestfire.py
-----------------
 This program highlights fires in an image by identifying
 pixels who red intensity is more than INTENSITY_THRESHOLD times
 the average of the red, green, and blue values at a pixel.
+----------------
 Those "sufficiently red" pixels are then highlighted in the
 image and the rest of the image is turned grey, by setting the
 pixels red, green, and blue values all to be the same average
@@ -25,7 +25,33 @@ def find_flames(filename):
     """
     image = SimpleImage(filename)
     # TODO: your code here
+    # Find pixels which are sufficiently red
+    # A pixel is sufficiently red if its red value is greater than 
+    # or equal to the avearage of the pixel's 3 RGB values
+    for pixel in image:
+        if is_sufficiently_red(pixel):
+            #
+            # Set pixel's red value to 255 and blue and gren values to 0 
+            # for pixels that are sufficiently red
+            pixel.red = 255
+            pixel.green = 0
+            pixel.blue = 0
+        else:    
+            #
+            # All other pixels, set their RGB values to grayscale equivalent
+            # by summing their RGB values, dividing by 3 and then setting their
+            # RGB alues to all have this same "average value."
+            average = (pixel.red + pixel.green + pixel.blue)/3
+            pixel.red = average
+            pixel.green = average
+            pixel.blue = average
+
     return image
+
+def is_sufficiently_red(pixel):
+    return pixel.red > (pixel.red + pixel.green + pixel.blue)/3
+
+
 
 def main():
     # Get file and load image
